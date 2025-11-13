fe6667b733abe33c6d9d2f2b6edfb52159d97bf1
MD ALTAF HOSSAIN SUNNY <www.altafhossainsunny1552@gmail.com>
2025-11-13 17:54:04 +0800

    Create gold layer tables in ddl_gold.sql

---
+++ scripts/gold /ddl_gold.sql
@@ -0,0 +1,86 @@
+IF OBJECT_ID('gold.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE gold.crm_cust_info;
+GO
+
+CREATE TABLE gold.crm_cust_info (
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_material_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE
+);
+GO
+
+-- CRM PRODUCT INFORMATION TABLE
+IF OBJECT_ID('gold.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE gold.crm_prd_info;
+GO
+
+CREATE TABLE gold.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line CHAR(10),
+    prd_start_dt DATE,
+    prd_end_dt DATE
+);
+GO
+
+-- CRM SALES DETAILS TABLE
+IF OBJECT_ID('gold.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE gold.crm_sales_details;
+GO
+
+CREATE TABLE gold.crm_sales_details (
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
+GO
+
+-- ERP CUSTOMER AZ12 TABLE
+IF OBJECT_ID('gold.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE gold.erp_cust_az12;
+GO
+
+CREATE TABLE gold.erp_cust_az12 (
+    CID NVARCHAR(50),
+    BDATE DATE,
+    GEN NVARCHAR(50)
+);
+GO
+
+-- ERP LOCATION A01 TABLE
+IF OBJECT_ID('gold.erp_loc_a01', 'U') IS NOT NULL
+    DROP TABLE gold.erp_loc_a01;
+GO
+
+CREATE TABLE gold.erp_loc_a01 (
+    CID NVARCHAR(50),
+    CNTRY NVARCHAR(50)
+);
+GO
+
+-- ERP PRODUCT CATEGORY G1V2 TABLE
+IF OBJECT_ID('gold.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE gold.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE gold.erp_px_cat_g1v2 (
+    ID NVARCHAR(50),
+    CAT NVARCHAR(50),
+    SUBCAT NVARCHAR(50),
+    MAINTENANCE NVARCHAR(50)
+);
+GO
+
+PRINT '✅ Gold layer tables created successfully!';
